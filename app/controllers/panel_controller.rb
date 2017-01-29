43415c5cbfd276d4883b90217d7bf94d9b7a4bc1
Tomohiro Murota <tomohiro.murota@gmail.com>
2017-01-29 17:37:18 +0900

    Add Digest authentication

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,4 +1,18 @@
+require 'digest/md5'
+
 class PanelController < ApplicationController
+  REALM = 'line_control_panel'
+  before_action :digest_auth
+
   def index
+  end
+
+  private
+
+  def digest_auth
+    authenticate_or_request_with_http_digest(REALM) do |user|
+      return nil if user != ENV['ADMIN_USER']
+      Digest::MD5.hexdigest([user, REALM, ENV['ADMIN_PASS']].join(':'))
+    end
   end
 end
